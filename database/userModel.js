292aa8c5de8d3480a000ebe0d2359a00a9cf808f
giftyaustin <austingifty@gmail.com>
2023-07-22 17:44:03 +0530

    env for client uri

--- database/userModel.js
+++ database/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+dotenv.config()
 
 
 const userSchema = mongoose.Schema({
@@ -10,7 +12,7 @@ const userSchema = mongoose.Schema({
 
 userSchema.methods.sendJWT=function(req, res, next){
     const token = jwt.sign(this.id, process.env.JWT_SECRET)
-    res.cookie('token', token, {httpOnly: true, sameSite: 'none', domain:'.netlify.app', secure:true})
+    res.cookie('token', token, {httpOnly: true, sameSite: 'none', domain:process.env.CLIENT_DOMAIN, secure:true})
     res.status(200).json({
       success:true,
       message: 'token sent'
--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const app = express()
 dbConnection();
 
 const cors = require('cors')
-corsOptions={origin:'https://beta--animated-sfogliatella-75e14b.netlify.app', credentials:true}
+corsOptions={origin: process.env.CLIENT_URI, credentials:true}
 app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://beta--animated-sfogliatella-75e14b.netlify.app');
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URI);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
