825ccb602a396cc8d941f8d620b14669d9cff90f
giftyaustin <austingifty@gmail.com>
2023-07-22 15:25:50 +0530

    fixed client route

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const app = express()
 dbConnection();
 
 const cors = require('cors')
-corsOptions={origin:'http://localhost:3000', credentials:true}
+corsOptions={origin:'https://memebait.onrender.com', credentials:true}
 app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://memebait.onrender.com');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
