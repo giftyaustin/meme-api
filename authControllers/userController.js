0559fec6625eceedcffd4b48ef559724eaf46576
giftyaustin <113115741+giftyaustin@users.noreply.github.com>
2023-07-23 18:49:20 +0530

    Update userController.js

--- authControllers/userController.js
+++ authControllers/userController.js
@@ -1,7 +1,7 @@
 const { tryCatch } = require("../errorHandling/tryCatch");
 
 exports.setGuest = tryCatch(async (req, res, next)=>{
-    res.cookie('token', 'q', {httpOnly:true, sameSite:'none', domain:'localhost', expries:Date(Date.now())})
+    res.cookie('token', 'q', {httpOnly:true, sameSite:'none', secure:true, expries:Date(Date.now())})
     res.json({
         success:true,
     })
