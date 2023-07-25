b5dc53d844ff983ee917f1b63d9035e06ca6083d
giftyaustin <austingifty@gmail.com>
2023-07-25 17:05:13 +0530

    fixed signup logic

--- authControllers/signupController.js
+++ authControllers/signupController.js
@@ -6,14 +6,17 @@ const { tryCatch } = require('../errorHandling/tryCatch')
 
 exports.signupUser = tryCatch(async (req, res, next)=>{
     const {username, password} = req.body
-    const user = User.findOne({username:username})
+    const user = await User.findOne({username:username})
     if(user){
         res.status(401).json({
             success:false,
             message: "Username already exists"
         })
     }
-    const newUser = await  User.create({username:username, password:password})
-    await newUser.save()
-    newUser.sendJWT(req, res, next)
+    else{
+        const newUser = await  User.create({username:username, password:password})
+        await newUser.save()
+        newUser.sendJWT(req, res, next)
+    }
+    
 })
